26da544cf682da7bb8f70c524135a694dac967bb
zeevmanilovich <zeev.manilovich@ironsrc.com>
2017-01-27 19:10:54 +0200

    updating generate doc script

--- generate_documentation.sh
+++ generate_documentation.sh
@@ -5,10 +5,10 @@ cd ./docs
 make html
 
 TARGET_BRANCH="gh-pages"
-mkdir $TARGET_BRANCH
-cd $TARGET_BRANCH
+mkdir ${TARGET_BRANCH}
+cd ${TARGET_BRANCH}
  
-git clone -b $TARGET_BRANCH --single-branch https://github.com/ironSource/atom-python.git
+git clone -b ${TARGET_BRANCH} --single-branch https://github.com/ironSource/atom-python.git
 
 cd atom-python
 cp -r ../../build/html/* .
@@ -17,7 +17,7 @@ git add .
 git commit -m "Deploy to GitHub Pages"
 
 # Now that we're all set up, we can push.
-git push origin $TARGET_BRANCH
+git push origin ${TARGET_BRANCH}
 
 cd ../../
-rm -r -f $TARGET_BRANCH
+rm -r -f ${TARGET_BRANCH}
